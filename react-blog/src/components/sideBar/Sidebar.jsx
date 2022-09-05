d53220e428b327993e91336214dbc0c8ade1bd84
Tafita Hery <tafitahery@gmail.com>
2022-09-05 17:35:26 +0300

    React sidebar example

--- react-blog/src/components/sideBar/Sidebar.jsx
+++ react-blog/src/components/sideBar/Sidebar.jsx
@@ -1,5 +1,39 @@
 import './sidebar.css';
 
 export default function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://cdn.pixabay.com/photo/2016/03/12/15/14/woman-1252185_960_720.jpg"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          aliquam ipsum rerum quas dicta minima dolorem fugiat.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
