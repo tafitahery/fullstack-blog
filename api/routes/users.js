4aa348dbf150de113244f05fd85b600813f6d0c7
Tafita Hery <tafitahery@gmail.com>
2022-09-07 20:39:39 +0300

    Node.js Post CRUD

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const User = require('../models/User');
 const Post = require('../models/Post');
 const bcrypt = require('bcrypt');
-const { json } = require('express');
 
 //UPDATE
 router.put('/:id', async (req, res) => {
