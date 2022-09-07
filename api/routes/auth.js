7318f5f3b2b0b8564643c679df23513a492489c4
Tafita Hery <tafitahery@gmail.com>
2022-09-07 19:33:51 +0300

    Node.js MongoDB User CRUD

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 router.post('/register', async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
-    const hashPassord = await bcryct.hash(req.body.password, salt);
+    const hashPassord = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
