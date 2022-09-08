ace9a7781961652bcc615c2dd62081f2cedb4901
Tafita Hery <tafitahery@gmail.com>
2022-09-08 06:19:56 +0300

    Node.js Request Query

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -69,9 +69,22 @@ router.get('/:id', async (req, res) => {
 
 //GET ALL POSTS
 router.get('/', async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(json);
   }
