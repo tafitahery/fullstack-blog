798a90947d46e7e99a18f31c17fb669591ebc760
Tafita Hery <tafitahery@gmail.com>
2022-09-08 17:24:39 +0300

    Displaying all post on the home page

--- react-blog/src/components/post/Post.jsx
+++ react-blog/src/components/post/Post.jsx
@@ -1,41 +1,27 @@
 import './post.css';
 import { Link } from 'react-router-dom';
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
         <span className="postTitle">
-          <Link className="link" to="/post/123">
-            Lorem ipsum dolor sit amet
+          <Link className="link" to={`/post/${post._id}`}>
+            {post.title}
           </Link>
         </span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-        nobis animi dicta temporibus exercitationem consequatur hic consequuntur
-        voluptas perspiciatis quibusdam. Laborum consequatur dolorem iste sint
-        laudantium perferendis, cumque consequuntur praesentium! Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Doloremque nobis animi
-        dicta temporibus exercitationem consequatur hic consequuntur voluptas
-        perspiciatis quibusdam. Laborum consequatur dolorem iste sint laudantium
-        perferendis, cumque consequuntur praesentium! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Doloremque nobis animi dicta temporibus
-        exercitationem consequatur hic consequuntur voluptas perspiciatis
-        quibusdam. Laborum consequatur dolorem iste sint laudantium perferendis,
-        cumque consequuntur praesentium!
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
