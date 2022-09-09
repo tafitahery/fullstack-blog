f3d6ecace62fa7bb600a817bcf55c277e0765484
Tafita Hery <tafitahery@gmail.com>
2022-09-09 16:22:19 +0300

    React Node.js Register System

--- react-blog/src/pages/register/Register.jsx
+++ react-blog/src/pages/register/Register.jsx
@@ -1,28 +1,52 @@
 import './register.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const { data } = await axios.post('/auth/register', {
+        username,
+        email,
+        password,
+      });
+      data && window.location.replace('/login');
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter your username"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           type="email"
           className="registerInput"
           placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           type="password"
           className="registerInput"
           placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className="registerButton">Register</button>
       </form>
@@ -31,6 +55,11 @@ function Register() {
           Login
         </Link>
       </button>
+      {error && (
+        <span style={{ color: 'red', marginTop: '10px' }}>
+          Something went wrong
+        </span>
+      )}
     </div>
   );
 }
