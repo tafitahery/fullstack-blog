c150ed9a355ec1bcd72ca85310a38e522afe46c8
Tafita Hery <tafitahery@gmail.com>
2022-09-18 20:59:51 +0300

    React delete post

--- react-blog/src/components/singlePost/SinglePost.jsx
+++ react-blog/src/components/singlePost/SinglePost.jsx
@@ -1,12 +1,22 @@
 import axios from 'axios';
+import { useContext } from 'react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Context } from '../../context/Context';
 import './singlePost.css';
 
 export default function SinglePost() {
   const { id } = useParams();
   const [post, setPost] = useState({});
   const PF = 'http://localhost:5000/images/';
+  const { user } = useContext(Context);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete('/posts/' + id, { data: { username: user.username } });
+      window.location.replace('/');
+    } catch (err) {}
+  };
 
   useEffect(() => {
     const getPost = async () => {
@@ -23,10 +33,15 @@ export default function SinglePost() {
         )}
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-            <i className="singlePostIcon fa-regular fa-trash-can"></i>
-          </div>
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+              <i
+                className="singlePostIcon fa-regular fa-trash-can"
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
