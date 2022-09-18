12f909627fb66b91cacd63e8154445966963abfa
Tafita Hery <tafitahery@gmail.com>
2022-09-18 19:47:17 +0300

    Creating new post using Axios

--- react-blog/src/pages/write/Write.jsx
+++ react-blog/src/pages/write/Write.jsx
@@ -1,6 +1,24 @@
+import { useContext, useState } from 'react';
+import axios from 'axios';
 import './write.css';
+import { Context } from '../../context/Context';
 
 function Write() {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    await axios.post('/posts');
+  };
+
   return (
     <div className="write">
       <img
@@ -8,7 +26,7 @@ function Write() {
         src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt=""
       />
-      <form className="writeForm">
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className="writeIcon fa-solid fa-plus"></i>
@@ -28,7 +46,9 @@ function Write() {
             className="writeInput writeText"
           ></textarea>
         </div>
-        <button className="writeSubmit">Publish</button>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
       </form>
     </div>
   );
